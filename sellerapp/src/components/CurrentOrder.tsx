245657445ac73a30d00e86269fcdd0f17b90502b
zar5789 <112923866+zar5789@users.noreply.github.com>
2023-10-05 13:20:10 +0700

    update something?

--- sellerapp/src/components/CurrentOrder.tsx
+++ sellerapp/src/components/CurrentOrder.tsx
@@ -1,95 +1,122 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 interface CurrentOrderProps {
   selectedOrder: number | null;
   onOrderClick: (index: number | null) => void;
 }
 
-interface CurrentOrderItem {
-  orderQueue: number;
-  menuName: string;
-  menuQuantity: number;
-  menuPrice: number;
-  paymentStatus: string;
+interface OrderData {
+  _id: string;
+  productID: string;
+  userID: string;
+  storeID: string;
+  amount: number;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+interface MenuData {
+  _id: string;
+  name: string;
+  price: number;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
 }
 
 export const CurrentOrder: React.FC<CurrentOrderProps> = ({
   selectedOrder,
   onOrderClick,
 }) => {
-  
-  const [currentOrders, setCurrentOrders] = useState<CurrentOrderItem[]>([
-    {
-      orderQueue: 1,
-      menuName: "กระเพราหมู",
-      menuQuantity: 2,
-      menuPrice: 50,
-      paymentStatus: "ชำระเงินแล้ว",
-    },
-    {
-      orderQueue: 2,
-      menuName: "ข้าวผัดไก่",
-      menuQuantity: 3,
-      menuPrice: 60,
-      paymentStatus: "จ่ายหน้าร้าน",
-    },
-    {
-      orderQueue: 3,
-      menuName: "ต้มยำกุ้ง",
-      menuQuantity: 1,
-      menuPrice: 70,
-      paymentStatus: "จ่ายหน้าร้าน",
-    },
-    {
-      orderQueue: 4,
-      menuName: "ต้มยำกุ้ง",
-      menuQuantity: 1,
-      menuPrice: 70,
-      paymentStatus: "จ่ายหน้าร้าน",
-    },
-    {
-      orderQueue: 5,
-      menuName: "ต้มยำกุ้ง",
-      menuQuantity: 1,
-      menuPrice: 70,
-      paymentStatus: "จ่ายหน้าร้าน",
-    },
-  ]);
+  const [orderDetails, setOrderDetails] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    // Fetch order data from the first API
+    fetch("https://order-api-patiparnpa.vercel.app/orders")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(async (orderData: OrderData[]) => {
+        const orderDetailsPromises = orderData.map(async (order) => {
+          // For each order, fetch the corresponding menu data from the second API
+          const menuResponse = await fetch(
+            `https://order-api-patiparnpa.vercel.app/products/${order.productID}`
+          );
+          if (!menuResponse.ok) {
+            throw new Error("Failed to fetch menu details");
+          }
+          const menuData: MenuData = await menuResponse.json();
+
+          // Combine order data and menu data
+          return {
+            orderQueue: orderData.indexOf(order) + 1,
+            menuName: menuData.name,
+            menuQuantity: order.amount,
+            menuPrice: (menuData.price * order.amount),
+            paymentStatus: order.status,
+          };
+        });
+
+        // Wait for all order details requests to complete
+        const details = await Promise.all(orderDetailsPromises);
+
+        setOrderDetails(details);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching order data:", error);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div className="current-order-container">
       <h2 className="current-order-title">ออเดอร์ที่กำลังทำ</h2>
       <div className="current-order-list">
-        {currentOrders.map((order, index) => (
-          <div
-            className={`current-order-item ${
-              order.paymentStatus === "ชำระเงินแล้ว"
-                ? "green-border"
-                : "yellow-border"
-            } ${index === selectedOrder ? "selected" : ""}`}
-            key={index}
-            onClick={() => onOrderClick(index === selectedOrder ? null : index)}
-          >
-            <h2 className="current-order-queue">{order.orderQueue}</h2>
-            <div className="current-menu-details">
-              <p className="current-menu-name">{order.menuName}</p>
-              <p className="current-menu-quantity">{order.menuQuantity} จาน</p>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          orderDetails.map((order, index) => (
+            <div
+              className={`current-order-item ${
+                order.paymentStatus === "Open"
+                  ? "green-border"
+                  : "yellow-border"
+              } ${index === selectedOrder ? "selected" : ""}`}
+              key={index}
+              onClick={() =>
+                onOrderClick(index === selectedOrder ? null : index)
+              }
+            >
+              <h2 className="current-order-queue">{order.orderQueue}</h2>
+              <div className="current-menu-details">
+                <p className="current-menu-name">{order.menuName}</p>
+                <p className="current-menu-quantity">
+                  {order.menuQuantity} จาน
+                </p>
+              </div>
+              <div className="current-menu-details">
+                <p
+                  className={`current-payment-status ${
+                    order.paymentStatus === "Open" ? "green" : "red"
+                  }`}
+                >
+                  {order.paymentStatus === "Open"
+                    ? "Open"
+                    : "Closed"}
+                </p>
+                <p className="current-menu-price">{order.menuPrice} บาท</p>
+              </div>
             </div>
-            <div className="current-payment">
-              <p
-                className={`current-payment-status ${
-                  order.paymentStatus === "ชำระเงินแล้ว" ? "green" : "red"
-                }`}
-              >
-                {order.paymentStatus === "ชำระเงินแล้ว"
-                  ? "ชำระเงินแล้ว"
-                  : "จ่ายหน้าร้าน"}
-              </p>
-              <p className="current-menu-price">{order.menuPrice} บาท</p>
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
